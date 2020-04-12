cc0d5d21bc4e750f05fea2d9b086ebe61844102c
Erv16 <epalani1@binghamton.edu>
2020-04-12 01:07:35 -0400

    Created comment.js file, added in comment schema and created the model. Updated the campground model to incorporate comments and added the comment template to the show route

--- app.js
+++ app.js
@@ -1,54 +1,76 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+const seedDB = require('./seeds');
 
+seedDB();
+mongoose.connect('mongodb://localhost/yelp_camp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-let campgrounds = [
-  {
-    name: 'Salmon Creek',
-    image:
-      'https://image.shutterstock.com/image-photo/sunbeam-morning-around-camping-site-260nw-649280029.jpg'
-  },
-  {
-    name: 'Granite Hill',
-    image:
-      'https://australia.businessesforsale.com/australian/static/articleimage?articleId=12982&name=image2.jpg'
-  },
-  {
-    name: 'Mountain Goats Coast',
-    image:
-      'https://i0.wp.com/campingseasonsupply.com/wp-content/uploads/2019/07/50.png?w=980&ssl=1'
-  }
-];
-
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('landing');
 });
 
-app.get('/campgrounds', function(req, res) {
-  res.render('campgrounds', { campgrounds: campgrounds });
+// shows all campgrounds
+app.get('/campgrounds', function (req, res) {
+  // Get campgrounds from db
+  Campground.find({}, function (err, allCampgrounds) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('index.ejs', { campgrounds: allCampgrounds });
+    }
+  });
 });
 
-app.post('/campgrounds', function(req, res) {
+// making a new campground and redirecting
+app.post('/campgrounds', function (req, res) {
   // get data from form and add to campgrounds
-
   let name = req.body.name;
   let image = req.body.image;
+  let desc = req.body.description;
   let newCampground = {
     name: name,
-    image: image
+    image: image,
+    description: desc,
   };
-  campgrounds.push(newCampground);
-  // redirect back to campgrounds page
-  res.redirect('/campgrounds');
+  // Create a new campground and save to database
+  Campground.create(newCampground, function (err, newlyCreated) {
+    if (err) {
+      console.log(err);
+    } else {
+      // redirect back to campgrounds page
+      res.redirect('/campgrounds');
+    }
+  });
 });
 
-app.get('/campgrounds/new', function(req, res) {
+// form for making a new campground
+app.get('/campgrounds/new', function (req, res) {
   res.render('new.ejs');
 });
 
-app.listen(process.env.PORT || 3000, process.env.IP, function() {
+// Show - shows more info about campground
+app.get('/campgrounds/:id', function (req, res) {
+  // Find the campground with provided id
+  Campground.findById(req.params.id)
+    .populate('comments')
+    .exec(function (err, foundCampground) {
+      if (err) {
+        console.log(err);
+      } else {
+        // Render show template with campground
+        res.render('show.ejs', { campground: foundCampground });
+      }
+    });
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP, function () {
   console.log('The YelpCamp server has started!');
 });
