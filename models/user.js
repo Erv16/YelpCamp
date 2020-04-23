5c65c4909119091cba19b5539d8bf4af9ad8cd3b
Erv16 <epalani1@binghamton.edu>
2020-04-23 16:13:25 -0400

    Added Password reset functionality

--- models/user.js
+++ models/user.js
@@ -4,6 +4,12 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const UserSchema = new mongoose.Schema({
   username: String,
   passowrd: String,
+  avatar: String,
+  firstName: String,
+  lastName: String,
+  email: { type: String, unique: true, required: true },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
   isAdmin: { type: Boolean, default: false },
 });
 
